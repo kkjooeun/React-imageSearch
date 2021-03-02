813a1d7f44762dadf55c5b24c7a53153fc7bba86
kkjooeun <zero5309405@gmail.com>
2021-03-02 22:23:38 +0900

    검색어입력창이있는 SearchBar 컴포넌트 생성

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,11 @@
 import React from 'react';
+import SearchBar from './SearchBar';
 
 const App = () => {
 	return(
-		<div>안녕?</div>
+		<div>
+			<SearchBar/>
+		</div>
 	)
 }
 
---
+++ src/components/SearchBar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default class SearchBar extends React.Component {
+	render() {
+		return (
+			<div>
+				<form>
+					<label>검색어를 입력하세요.</label>
+					<input/>
+				</form>
+			</div>
+		)
+	}
+}
