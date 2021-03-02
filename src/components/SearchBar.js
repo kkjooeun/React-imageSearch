278ed427f4a23b9c41740867eced38cd2940dbac
kkjooeun <zero5309405@gmail.com>
2021-03-02 22:38:05 +0900

    SarchBar input입력값 state로 관리

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,12 +1,24 @@
 import React from 'react';
 
 export default class SearchBar extends React.Component {
+	state = {
+		query : "",
+	}
+
+	onInputChange = (e) => {
+		this.setState({query:e.target.value});
+	}
+
 	render() {
 		return (
 			<div>
 				<form className='ui form'>
 					<label>검색어를 입력하세요.</label>
-  				<input type="text"/>
+  				<input 
+						type="text"
+						value={this.state.query}
+						onChange={this.onInputChange}
+					/>
 				</form>
 			</div>
 		)
