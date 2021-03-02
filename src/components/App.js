7e28469e84ebef1428966c82004aec49e91d5b20
kkjooeun <zero5309405@gmail.com>
2021-03-02 22:48:16 +0900

    사용자 입력값 unsplash api promise로 구현

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
 const App = () => {
+	const [images, setImages] = useState([]);
+
+	const onSubmit = (query) => {
+		const url = 'https://api.unsplash.com/search/photos'
+		axios.get(url, {
+			params : {
+				query
+			},
+			headers: {
+				Authorization : 'Client-ID 12fNUeS0axgg8SEeJpBoJpOSLF_uNk-JSwKmWSYaBS8'
+			}
+		}).then((res) => {
+			console.log(res);
+			setImages(res.data.results);
+		});
+	}
+
 	return(
 		<div className='ui container'>
-			<SearchBar/>
+			<SearchBar onSubmit={onSubmit}/>
 		</div>
 	)
 }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -9,10 +9,15 @@ export default class SearchBar extends React.Component {
 		this.setState({query:e.target.value});
 	}
 
+	onFormSubmit = (e) => {
+		e.preventDefault();
+		this.props.onSubmit(this.state.query);
+	}
+
 	render() {
 		return (
 			<div>
-				<form className='ui form'>
+				<form className='ui form' onSubmit={this.onFormSubmit}>
 					<label>검색어를 입력하세요.</label>
   				<input 
 						type="text"
